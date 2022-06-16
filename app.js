75fd995e1a6030e662b6d1ae0af6769f26a18e51
Janis Mangold <mangolja@hs-albsig.de>
2022-06-16 11:10:32 +0200

    guestbook api

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+const swaggerUI = require('swagger-ui-dist')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index')
+var apiRouter = require('./routes/users')
 
-var app = express();
+var app = express()
+var port = 8000
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
+app.use('/api/v1', apiRouter)
 
-module.exports = app;
+//Deliver the swagger-ui relevant JS libs
+app.use('/swagger-ui/', express.static(swaggerUI.getAbsoluteFSPath()))
+
+app.listen(port, ()=>{
+    console.log(`App listening on port ${port}`);
+})
+module.exports = app
